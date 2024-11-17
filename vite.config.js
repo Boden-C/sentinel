48c56cf468db6d2ec5943b6aad5a4d6ea28e548c
Boden-C <70297682+Boden-C@users.noreply.github.com>
2024-11-16 22:36:03 -0600

    Changed configs

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import vitePluginRequire from 'vite-plugin-require';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), vitePluginRequire(),],
+    plugins: [react()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
